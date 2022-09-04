4a94e1317cf3c1a7bbbb9b961d77cb62f51728f5
YingrongChen <ych2429@emory.edu>
2022-09-03 23:33:32 -0400

    complete first draft with 2 test cases

--- Run.java
+++ Run.java
@@ -2,22 +2,29 @@
 public class Run {
 
 	public static void main(String[] args) {
-		Patient p = new Patient();
-		Functions f = new Functions();
-		
-		if(f.diagnose(p)) {
-			if (f.treatment(p)) {
-				System.out.println(f.postTreatment(p));
-			}
-			else {
-				System.out.println(f.treatment(p));
-			}
-		}
-		else {
-			System.out.println(f.diagnose(p));
-		}
+
+		Patient p = new Patient(false, false, 20, true, false, true);
+		if(p.getPregnant()) p.setTrimester(1);
+		p.inputTFT(0.18, 1.3, 2.7);
+		p.inputAntibody(true, 1.4);
+		p.tracker(50, 75, true);
+		p.tracker(52, 80, false);
+		p.tracker(55, 88, false);
+
+		boolean result;
+		if(p.getOnMedication()) result = p.diagnosis();
+		else result = p.remission();
+
+		Patient p1 = new Patient(false, true, 17, false, true, false);
+		if(p.getPregnant()) p.setTrimester(1);
+		p.inputTFT(0.18, 1.3, 2.7);
+		p.inputAntibody(true, 1.4);
+		p.tracker(50, 75, true);
+		p.tracker(52, 80, false);
+		p.tracker(55, 88, false);
+
+		boolean result1;
+		if(p.getOnMedication()) result1 = p.diagnosis();
+		else result1 = p.remission();
 	}
-	
-	
-	
 }
